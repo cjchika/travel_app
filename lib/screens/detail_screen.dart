c015859f70ff13e7b7fc945c95bb670ce489fae3
Chijioke Chika <8pluscreative@gmail.com>
2023-05-13 07:00:32 +0100

    Detail Screen

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -16,7 +16,33 @@ class _DetailScreenState extends State<DetailScreen> {
       body: Container(
         width: double.maxFinite,
         height: double.maxFinite,
-        child: Stack(),
+        child: Stack(children: [
+          Positioned(
+              width: 380,
+              left: 20,
+              top: 50,
+              child: Row(
+                // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  IconButton(onPressed: () {}, icon: const Icon(Icons.menu)),
+                  Spacer(),
+                  IconButton(
+                      onPressed: () {}, icon: const Icon(Icons.more_vert))
+                ],
+              )),
+          Positioned(
+              left: 0,
+              right: 0,
+              child: Container(
+                width: double.maxFinite,
+                height: 300,
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('img/mountain.jpeg'),
+                  ),
+                ),
+              ))
+        ]),
       ),
     );
   }
