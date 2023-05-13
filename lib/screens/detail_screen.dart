b495f1bfa4e790b8346354b347938cdb2d92cd35
Chijioke Chika <8pluscreative@gmail.com>
2023-05-13 08:09:23 +0100

    Detail Screen

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:travel_app/misc/colors.dart';
+import 'package:travel_app/widgets/app_large_text.dart';
+import 'package:travel_app/widgets/app_text.dart';
 
 class DetailScreen extends StatefulWidget {
   const DetailScreen({super.key});
@@ -52,14 +55,56 @@ class _DetailScreenState extends State<DetailScreen> {
           Positioned(
               top: 300,
               child: Container(
+                padding: const EdgeInsets.only(left: 20, right: 20, top: 30),
                 height: 550,
                 width: MediaQuery.of(context).size.width,
                 decoration: const BoxDecoration(
-                  color: Colors.red,
+                  color: Colors.white,
                   borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(40),
-                    topRight: Radius.circular(40),
+                    topLeft: Radius.circular(50),
+                    topRight: Radius.circular(50),
                   ),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        AppLargeText(
+                            text: "Yosemite",
+                            color: Colors.black.withOpacity(0.8)),
+                        AppLargeText(
+                            text: "\$ 240", color: AppColors.mainColor),
+                      ],
+                    ),
+                    const SizedBox(height: 10),
+                    Row(
+                      children: [
+                        Icon(
+                          Icons.location_on,
+                          color: AppColors.mainColor,
+                        ),
+                        const SizedBox(width: 5),
+                        AppText(
+                          text: "USA, California",
+                          color: AppColors.textColor1,
+                        )
+                      ],
+                    ),
+                    const SizedBox(height: 20),
+                    Row(
+                      children: [
+                        Wrap(
+                          children: List.generate(5, (index) {
+                            return Icon(Icons.star, color: AppColors.starColor);
+                          }),
+                        ),
+                        const SizedBox(width: 10),
+                        AppText(text: "(4.5)", color: AppColors.textColor2),
+                      ],
+                    )
+                  ],
                 ),
               ))
         ]),
