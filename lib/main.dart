bb0c95e7f99e3ec3ce31bc0bac39cb15c7f24348
Chijioke Chika <8pluscreative@gmail.com>
2023-05-14 09:57:04 +0100

    Cubit State Mgt

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:travel_app/cubit/app_cubit_logics.dart';
 import 'package:travel_app/cubit/app_cubits.dart';
-import 'package:travel_app/screens/detail_screen.dart';
-import 'package:travel_app/screens/navscreens/main_screen.dart';
-import 'package:travel_app/screens/welcome_screen.dart';
+// import 'package:travel_app/screens/detail_screen.dart';
+// import 'package:travel_app/screens/navscreens/main_screen.dart';
+// import 'package:travel_app/screens/welcome_screen.dart';
 
 void main() {
   runApp(const MyApp());
